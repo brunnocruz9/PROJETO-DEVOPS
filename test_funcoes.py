4123807c6e3cd170dbf810b89e1739f95e8f0455
brunnocruz9 <brunnokzng@gmail.com>
2025-04-27 16:36:57 -0300

    move: move os arquivos de teste para o repositório correto

---
+++ test_funcoes.py
@@ -0,0 +1,17 @@
+import pytest
+from funcoes import soma, subtrai, multiplica, divide, quadrado
+
+def test_soma():
+    assert soma(2, 3) == 5
+
+def test_subtrai():
+    assert subtrai(5, 3) == 2
+
+def test_multiplica():
+    assert multiplica(4, 3) == 12
+
+def test_divide():
+    assert divide(10, 2) == 5
+
+def test_quadrado():
+    assert quadrado(4) == 16
